f15d16804bdc0af7c56cf135b52c72522a7fc0c5
haohua Feng <haohuafeng08@gmail.com>
2020-09-20 19:30:44 -0400

    move test to package tests

--- src/tests/manualTest_ED.java
+++ src/tests/manualTest_ED.java
@@ -1,7 +1,9 @@
+package tests;
+
 import card_class.card;
 
 //this is not a unit test
-public class test {
+public class manualTest_ED {
     public static void main(String[] args) {
         card card = new card_class.card("STONE WALL");
         System.out.println("Name: " + card.getCard_name() +
