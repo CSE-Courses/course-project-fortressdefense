d2607085487c3c2dca770b1cbfa0c9639cef0e80
Haohua Feng <47614849+HaohuaFeng@users.noreply.github.com>
2020-10-12 07:40:20 -0400

    old

--- src/code/Socket/client.java
+++ src/code/Socket/client.java
@@ -7,16 +7,17 @@ import java.net.Socket;
 
 public class client {
     public static void main(String[] args) throws Exception {
-        Socket socket = new Socket("localhost", 10000);
+        Socket socket = new Socket("172.20.6.109", 16225);
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter out = new PrintWriter(socket.getOutputStream());
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         while (true) {
+            System.out.println(1);
             String msg = reader.readLine();
             out.println(msg);
             out.flush();
-            if (msg.equals("bye")) {
+            if (msg.equals("GAME OVER")) {
                 break;
             }
             System.out.println(in.readLine());
