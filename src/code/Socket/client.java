c772fd119d6843e708d84a0cc80c4f36520f99bb
haohua Feng <haohuafeng08@gmail.com>
2020-10-12 01:54:52 -0400

    test

--- src/code/Socket/client.java
+++ src/code/Socket/client.java
@@ -1,16 +1,13 @@
 package code.Socket;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
-
-import code.Deck.Player;
-import code.Socket.*;
-import code.*;
 
 public class client {
     public static void main(String[] args) throws Exception {
-        Socket socket = new Socket("localhost", 16225);
+        Socket socket = new Socket("localhost", 10000);
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter out = new PrintWriter(socket.getOutputStream());
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
--- src/code/Socket/server.java
+++ src/code/Socket/server.java
@@ -1,16 +1,15 @@
 package code.Socket;
 
-import java.io.*;
-import java.net.*;
-import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class server {
-
     public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(16225);
-
+        ServerSocket server = new ServerSocket(10000);
         while (true) {
             Socket socket = server.accept();
             invoke(socket);
